de37e5fc42723e69e95b4395d533e1d5e839e353
Darren Ethier <darren@roughsmootheng.in>
2012-06-18 11:06:00 -0400

    [completed: 15]

--- organize-series-shortcodes.php
+++ organize-series-shortcodes.php
@@ -10,7 +10,7 @@ Author URI: http://organizeseries.com
 $os_shortcodes_ver = '1.3';
 
 /* LICENSE */
-//"Organize Series Plugin" and all addons for it created by this author are copyright (c) 2007-2011 Darren Ethier. This program is free software; you can redistribute it and/or
+//"Organize Series Plugin" and all addons for it created by this author are copyright (c) 2007-2012 Darren Ethier. This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
 // as published by the Free Software Foundation; either version 2
 // of the License, or (at your option) any later version.
