cb1a3d421e8a751adb5fe5e1308c3f480c631a7f
Darren Ethier <darren@roughsmootheng.in>
2018-03-25 18:01:59 -0400

    add info.json and initialize rc versioning

--- organize-series-shortcodes.php
+++ organize-series-shortcodes.php
@@ -2,12 +2,12 @@
 /*
 Plugin Name: Organize Series Shortcodes
 Description: This addon enables the ability for users to easily add series information to posts (or pages) via the use of shortcodes (integrated with the WordPress shortcode API).  
-Version: 1.3.2
+Version: 1.3.3.rc.000
 Author: Darren Ethier
 Author URI: http://organizeseries.com
 */
 
-$os_shortcodes_ver = '1.3.2';
+$os_shortcodes_ver = '1.3.3.rc.000';
 require __DIR__ . '/vendor/autoload.php';
 
 /* LICENSE */
